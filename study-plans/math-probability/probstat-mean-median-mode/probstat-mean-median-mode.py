6597a974ccbd961a588128b87de4dfccb76a4df6
Junle (Juno) Zhou <99487181+Lexian-6@users.noreply.github.com>
2026-05-06 09:48:53 +1000

    Update solution for probstat-mean-median-mode

--- study-plans/math-probability/probstat-mean-median-mode/probstat-mean-median-mode.py
+++ study-plans/math-probability/probstat-mean-median-mode/probstat-mean-median-mode.py
@@ -6,6 +6,4 @@ def mean_median_mode(x):
     Returns: dict with 'mean', 'median', 'mode' as floats.
     """
     counts = Counter(x)
-    for i in counts:
-        if counts[i] == max(counts.values()):
-            return {'mean': np.mean(x), 'median': np.median(x), 'mode': i}
+    return {'mean': np.mean(x), 'median': np.median(x), 'mode': max(counts, key=lambda x: counts[x])}

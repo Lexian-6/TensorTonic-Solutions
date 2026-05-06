89081fb8642716ad98a9c0ed5f0feede38b26bc7
Junle (Juno) Zhou <99487181+Lexian-6@users.noreply.github.com>
2026-05-06 10:10:33 +1000

    Add solution for probstat-percentiles

---
+++ study-plans/math-probability/probstat-percentiles/probstat-percentiles.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def percentiles(x, q):
+    """
+    Returns: numpy array of percentile values.
+    """
+    return np.array([np.percentile(x, quantile) for quantile in q], dtype=float)
